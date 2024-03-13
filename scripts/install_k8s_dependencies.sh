35642cf667eb61af17e065ca720d8aaf8ed71e7e
Olaf Radicke <briefkasten@olaf-radicke.de>
2024-03-13 14:25:25 +0100

    Fix pipeline.

--- scripts/install_k8s_dependencies.sh
+++ scripts/install_k8s_dependencies.sh
@@ -6,5 +6,6 @@ set -u
 
 go get github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes@latest || true
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+cd ./pulumi/
 go mod tidy
 mkdir ~/.kube
